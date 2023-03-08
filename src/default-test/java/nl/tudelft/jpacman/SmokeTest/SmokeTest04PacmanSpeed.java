330dd578bd326fb34733c02821ea6cd1a56b94a8
athenexfolk <folkadpocket99@gmail.com>
2023-03-09 06:09:50 +0700

    Add some comment

--- src/default-test/java/nl/tudelft/jpacman/SmokeTest/SmokeTest04PacmanSpeed.java
+++ src/default-test/java/nl/tudelft/jpacman/SmokeTest/SmokeTest04PacmanSpeed.java
@@ -1,29 +1,50 @@
 package nl.tudelft.jpacman.SmokeTest;
 
-import jdk.jfr.Percentage;
 import nl.tudelft.jpacman.Launcher;
 import nl.tudelft.jpacman.game.Game;
 import nl.tudelft.jpacman.level.Player;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * End-To-End Test how to ensure that pacman have constant speed
+ * by checking score of eating pellets when press a key in any action
+ *
+ * @author Saksit Sirisakda
+ */
 public class SmokeTest04PacmanSpeed {
+    /**
+     * The game launcher for init the game
+     */
     private Launcher launcher;
+    /**
+     * The robot for making mouse and press event
+     */
     private Robot bot;
+    /**
+     * Period of eating pellets
+     */
     private long intervalTime = 500;
+    /**
+     * Total score that Pacman do in <code>IntervalTime</code>
+     */
     private int scoreInIntervalTime = 50;
-    private long delayAfterKeyup = 2000;
+    /**
+     * Error of score in one period of time
+     */
     private int scoreEpsilon = 30;
+
+    /**
+     * Open the game
+     * @throws AWTException
+     */
     @BeforeEach
     void setUp() throws AWTException{
         launcher = new Launcher();
@@ -37,6 +58,12 @@ public class SmokeTest04PacmanSpeed {
     void tearDown(){
         launcher.dispose();
     }
+
+    /**
+     * Player score should always equal the expected score
+     * or in interval error when press key once
+     * @throws InterruptedException
+     */
     @Test
     void pacmanSpeedTest()
         throws InterruptedException{
@@ -45,10 +72,16 @@ public class SmokeTest04PacmanSpeed {
         Player player = game.getPlayers().get(0);
 
         clickStartBtn();
-        enterKey(KeyEvent.VK_RIGHT,delayAfterKeyup);
+        enterKey(KeyEvent.VK_RIGHT);
+        Thread.sleep(intervalTime);
 
         assertEquals(scoreInIntervalTime,player.getScore(),scoreEpsilon);
     }
+    /**
+     * Player score should always equal the expected score
+     * or in interval error when press key many times
+     * @throws InterruptedException
+     */
     @Test
     void pacmanSpeedTestInterval() throws InterruptedException{
         launcher.withMapFile("/testBoard/board.txt").launch();
@@ -56,7 +89,7 @@ public class SmokeTest04PacmanSpeed {
         Player player = game.getPlayers().get(0);
 
         clickStartBtn();
-        enterKeyInterval(KeyEvent.VK_RIGHT,delayAfterKeyup);
+        enterKeyInterval(KeyEvent.VK_RIGHT);
 
         assertEquals(scoreInIntervalTime,player.getScore(),scoreEpsilon);
     }
@@ -74,21 +107,23 @@ public class SmokeTest04PacmanSpeed {
      * Simulate pressing key on keyboard
      *
      * @param key {@link KeyEvent} to simulate
-     * @param delayAfter delay after press key
      * @throws InterruptedException
      */
-    void enterKeyInterval(int key,long delayAfter) throws InterruptedException{
-        long a = System.currentTimeMillis() + intervalTime;
-        while(System.currentTimeMillis() < a){
+    void enterKeyInterval(int key) throws InterruptedException{
+        long timeInterval = System.currentTimeMillis() + intervalTime;
+        while(System.currentTimeMillis() < timeInterval){
             bot.keyPress(key);
         }
         bot.keyRelease(key);
-        Thread.sleep(delayAfter);
     }
 
-    void enterKey(int key,long delayAfter) throws InterruptedException{
+    /**
+     * Making <code>bot</code> press the key
+     * @param key
+     * @throws InterruptedException
+     */
+    void enterKey(int key) throws InterruptedException{
         bot.keyPress(key);
         bot.keyRelease(key);
-        Thread.sleep(delayAfter);
     }
 }
