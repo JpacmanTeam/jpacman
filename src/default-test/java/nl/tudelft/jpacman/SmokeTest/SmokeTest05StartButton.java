680d400346b6921a77394815baee076432f42876
Areeya Lakonpol <areyalak2001@gmail.com>
2023-03-10 01:55:41 +0700

    Fix test start button

--- src/default-test/java/nl/tudelft/jpacman/SmokeTest/SmokeTest05StartButton.java
+++ src/default-test/java/nl/tudelft/jpacman/SmokeTest/SmokeTest05StartButton.java
@@ -1,23 +1,33 @@
 package nl.tudelft.jpacman.SmokeTest;
 
 import nl.tudelft.jpacman.Launcher;
+import nl.tudelft.jpacman.board.Board;
+import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.game.Game;
+import nl.tudelft.jpacman.level.CollisionMap;
 import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Pellet;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.npc.Ghost;
+import nl.tudelft.jpacman.sprite.Sprite;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 import java.awt.*;
+import java.util.Collections;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.mockito.Mockito.mock;
 
 
 public class SmokeTest05StartButton {
 
     private Launcher launcher;
+    private Game game;
+    private Level level;
 
 
     /**
@@ -26,7 +36,9 @@ public class SmokeTest05StartButton {
     @BeforeEach
     public void before() {
         launcher = new Launcher();
+
     }
+
 
     /**
      * Close the user interface.
@@ -40,18 +52,27 @@ public class SmokeTest05StartButton {
 
 
     @Test
-    public void testRestartGameAfterFinish() {
+    public void OpenGame(){
         launcher.launch();
         getGame().start();
-
-        //Game needs to finish
-        assertThat(getGame().isInProgress()).isFalse();
-
+        assertThat(getGame().isInProgress()).isTrue();
     }
+
+    @Test
+    public void  TestNoPelletLeft() {
+        // check that there are no pellets left
+        // No pellets left = Finnish
+        assertThat(level.remainingPellets()).isZero();
+        //Game needs to finish
+        assertThat(level.isInProgress()).isTrue();
+    }
+
+
 
     private Game getGame() {
         return launcher.getGame();
     }
+
 }
 
 
