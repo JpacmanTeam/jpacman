9157682d8a7c1faa12f0ef6470be1eb168c55994
Villsource <anirut1800@gmail.com>
2023-03-10 21:20:13 +0700

    add PacmanUiBuilderTest

---
+++ src/default-test/java/nl/tudelft/jpacman/ui/PacmanUiBuilderTest.java
@@ -0,0 +1,18 @@
+package nl.tudelft.jpacman.ui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PacmanUiBuilderTest {
+
+    private PacManUiBuilder builder;
+
+    @BeforeEach void setUp(){
+
+    }
+
+    @Test void PacManUiBuilder(){
+        assert false;
+    }
+
+}
